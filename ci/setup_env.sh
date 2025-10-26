3f8bb76cf0e1e88b9881c5a75eee56cc3f258d8c
renatabagdanova <bagdanova.renata@gmail.com>
2025-10-26 14:20:07 +0300

    Commit 10

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 envs="
-apiAccessToken=${eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwcmFjdGljdW0ucnUiLCJhdWQiOiJwcmFjdGljdW0ucnUiLCJ1c2VybmFtZSI6InJlbmF0YS1iYWdkYW5vd2EifQ.jSM5jftkcwWYUE2iDsHYI7Mvytpzd4W0q9UuGA8dQlM}
+apiAccessToken=${GH_API_ACCESS_TOKEN}
 "
 
 (echo "$envs" | grep -E '.+=.+') >> develop.properties
